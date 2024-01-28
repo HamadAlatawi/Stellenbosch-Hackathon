0240c923248de715e36728059221c495e8c755aa
hamad <hamadalatawi@protonmail.com>
2024-01-28 17:08:22 +0300

    Added Drawer functionality to transactions.

--- src/frontend/src/routes/transaction/+page.svelte
+++ src/frontend/src/routes/transaction/+page.svelte
@@ -1,11 +1,39 @@
 <script lang="ts">
     import { onMount } from 'svelte';
+    import { Button } from "$lib/components/ui/button";
     import * as Table from "$lib/components/ui/table";
     import * as Pagination from "$lib/components/ui/pagination";
     import Skeleton from '$lib/components/ui/skeleton/skeleton.svelte';
+    import * as Drawer from "$lib/components/ui/drawer";
 
     let showSkeleton = true;
+    
 
+    let invoices = [
+        { invoiceNumber: 'INV001', status: 'Paid', method: 'Credit Card', amount: '$250.00' },
+        { invoiceNumber: 'INV002', status: 'Pending', method: 'PayPal', amount: '$150.00' },
+        { invoiceNumber: 'INV003', status: 'Unpaid', method: 'Bank Transfer', amount: '$350.00' },
+        { invoiceNumber: 'INV004', status: 'Paid', method: 'Credit Card', amount: '$450.00' },
+        { invoiceNumber: 'INV005', status: 'Paid', method: 'PayPal', amount: '$550.00' },
+        { invoiceNumber: 'INV006', status: 'Pending', method: 'Bank Transfer', amount: '$200.00' },
+        { invoiceNumber: 'INV007', status: 'Unpaid', method: 'Credit Card', amount: '$300.00' },
+        { invoiceNumber: 'INV008', status: 'Paid', method: 'Bank Transfer', amount: '$700.00' },
+        { invoiceNumber: 'INV009', status: 'Pending', method: 'Credit Card', amount: '$800.00' },
+        { invoiceNumber: 'INV010', status: 'Unpaid', method: 'PayPal', amount: '$900.00' },
+        { invoiceNumber: 'INV011', status: 'Paid', method: 'Bank Transfer', amount: '$1000.00' },
+        { invoiceNumber: 'INV012', status: 'Pending', method: 'Credit Card', amount: '$1100.00' },
+        { invoiceNumber: 'INV013', status: 'Unpaid', method: 'PayPal', amount: '$1200.00' },
+        { invoiceNumber: 'INV014', status: 'Paid', method: 'Bank Transfer', amount: '$1300.00' },
+        { invoiceNumber: 'INV015', status: 'Pending', method: 'Credit Card', amount: '$1400.00' },
+        { invoiceNumber: 'INV016', status: 'Unpaid', method: 'PayPal', amount: '$1500.00' },
+        { invoiceNumber: 'INV017', status: 'Paid', method: 'Bank Transfer', amount: '$1600.00' },
+        { invoiceNumber: 'INV018', status: 'Pending', method: 'Credit Card', amount: '$1700.00' },
+        { invoiceNumber: 'INV019', status: 'Unpaid', method: 'PayPal', amount: '$1800.00' },
+        { invoiceNumber: 'INV020', status: 'Paid', method: 'Bank Transfer', amount: '$1900.00' }
+    ];
+
+
+    
     onMount(() => {
         setTimeout(() => {
             showSkeleton = false;
@@ -21,26 +49,30 @@
                 <Table.Caption>A list of transactions</Table.Caption>
                 <Table.Header>
                     <Table.Row>
-                        <Table.Head class="w-[100px]">Invoice</Table.Head>
-                        <Table.Head>Status</Table.Head>
-                        <Table.Head>Method</Table.Head>
-                        <Table.Head class="text-right">Amount</Table.Head>
+                        <Table.Head class="w-1/5">Invoice</Table.Head>
+                        <Table.Head class="w-1/5">Status</Table.Head>
+                        <Table.Head class="w-1/5">Method</Table.Head>
+                        <Table.Head class="w-1/5">Amount</Table.Head>
+                        <Table.Head class="w-1/5 text-right">Transaction Details</Table.Head>
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>
                     {#each Array.from({ length: 20 }, (_, i) => i) as index}
                         <Table.Row>
                             <Table.Cell class="font-medium">
-                                <Skeleton class="w-[50px] h-[20px]" />
+                                <Skeleton class="w-[40%] h-[27px]" />
                             </Table.Cell>
                             <Table.Cell>
-                                <Skeleton class="w-[60px] h-[20px]" />
+                                <Skeleton class="w-[40%] h-[27px]" />
                             </Table.Cell>
                             <Table.Cell>
-                                <Skeleton class="w-[100px] h-[20px]" />
+                                <Skeleton class="w-[60%] h-[27px]" />
+                            </Table.Cell>
+                            <Table.Cell>
+                                <Skeleton class="w-[45%] h-[27px]" />
                             </Table.Cell>
                             <Table.Cell class="flex justify-end items-end">
-                                <Skeleton class="w-[60px] h-[20px]" />
+                                <Skeleton class="w-[62%] h-[27px]" />
                             </Table.Cell>
                         </Table.Row>
                     {/each}
@@ -78,137 +110,52 @@
             <Table.Root>
                 <Table.Caption>A list of transactions</Table.Caption>
                 <Table.Header>
-                <Table.Row>
-                    <Table.Head class="w-[100px]">Invoice</Table.Head>
-                    <Table.Head>Status</Table.Head>
-                    <Table.Head>Method</Table.Head>
-                    <Table.Head class="text-right">Amount</Table.Head>
-                </Table.Row>
+                    <Table.Row>
+                        <Table.Head class="w-1/5">Invoice</Table.Head>
+                        <Table.Head class="w-1/5">Status</Table.Head>
+                        <Table.Head class="w-1/5">Method</Table.Head>
+                        <Table.Head class="w-1/5">Amount</Table.Head>
+                        <Table.Head class="w-1/5 text-right">Transaction Details</Table.Head>
+                    </Table.Row>
                 </Table.Header>
                 <Table.Body>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV001</Table.Cell>
-                    <Table.Cell>Paid</Table.Cell>
-                    <Table.Cell>Credit Card</Table.Cell>
-                    <Table.Cell class="text-right">$250.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV002</Table.Cell>
-                    <Table.Cell>Pending</Table.Cell>
-                    <Table.Cell>PayPal</Table.Cell>
-                    <Table.Cell class="text-right">$150.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV003</Table.Cell>
-                    <Table.Cell>Unpaid</Table.Cell>
-                    <Table.Cell>Bank Transfer</Table.Cell>
-                    <Table.Cell class="text-right">$350.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV004</Table.Cell>
-                    <Table.Cell>Paid</Table.Cell>
-                    <Table.Cell>Credit Card</Table.Cell>
-                    <Table.Cell class="text-right">$450.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV005</Table.Cell>
-                    <Table.Cell>Paid</Table.Cell>
-                    <Table.Cell>PayPal</Table.Cell>
-                    <Table.Cell class="text-right">$550.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV006</Table.Cell>
-                    <Table.Cell>Pending</Table.Cell>
-                    <Table.Cell>Bank Transfer</Table.Cell>
-                    <Table.Cell class="text-right">$200.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV007</Table.Cell>
-                    <Table.Cell>Unpaid</Table.Cell>
-                    <Table.Cell>Credit Card</Table.Cell>
-                    <Table.Cell class="text-right">$300.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV008</Table.Cell>
-                    <Table.Cell>Paid</Table.Cell>
-                    <Table.Cell>Bank Transfer</Table.Cell>
-                    <Table.Cell class="text-right">$700.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV009</Table.Cell>
-                    <Table.Cell>Pending</Table.Cell>
-                    <Table.Cell>Credit Card</Table.Cell>
-                    <Table.Cell class="text-right">$800.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV010</Table.Cell>
-                    <Table.Cell>Unpaid</Table.Cell>
-                    <Table.Cell>PayPal</Table.Cell>
-                    <Table.Cell class="text-right">$900.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV011</Table.Cell>
-                    <Table.Cell>Paid</Table.Cell>
-                    <Table.Cell>Bank Transfer</Table.Cell>
-                    <Table.Cell class="text-right">$1000.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV012</Table.Cell>
-                    <Table.Cell>Pending</Table.Cell>
-                    <Table.Cell>Credit Card</Table.Cell>
-                    <Table.Cell class="text-right">$1100.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV013</Table.Cell>
-                    <Table.Cell>Unpaid</Table.Cell>
-                    <Table.Cell>PayPal</Table.Cell>
-                    <Table.Cell class="text-right">$1200.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV014</Table.Cell>
-                    <Table.Cell>Paid</Table.Cell>
-                    <Table.Cell>Bank Transfer</Table.Cell>
-                    <Table.Cell class="text-right">$1300.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV015</Table.Cell>
-                    <Table.Cell>Pending</Table.Cell>
-                    <Table.Cell>Credit Card</Table.Cell>
-                    <Table.Cell class="text-right">$1400.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV016</Table.Cell>
-                    <Table.Cell>Unpaid</Table.Cell>
-                    <Table.Cell>PayPal</Table.Cell>
-                    <Table.Cell class="text-right">$1500.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV017</Table.Cell>
-                    <Table.Cell>Paid</Table.Cell>
-                    <Table.Cell>Bank Transfer</Table.Cell>
-                    <Table.Cell class="text-right">$1600.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV018</Table.Cell>
-                    <Table.Cell>Pending</Table.Cell>
-                    <Table.Cell>Credit Card</Table.Cell>
-                    <Table.Cell class="text-right">$1700.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV019</Table.Cell>
-                    <Table.Cell>Unpaid</Table.Cell>
-                    <Table.Cell>PayPal</Table.Cell>
-                    <Table.Cell class="text-right">$1800.00</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell class="font-medium">INV020</Table.Cell>
-                    <Table.Cell>Paid</Table.Cell>
-                    <Table.Cell>Bank Transfer</Table.Cell>
-                    <Table.Cell class="text-right">$1900.00</Table.Cell>
-                </Table.Row>
+                    {#each invoices as invoice}
+                        <Table.Row>
+                            <Table.Cell class="font-medium">{invoice.invoiceNumber}</Table.Cell>
+                            <Table.Cell>{invoice.status}</Table.Cell>
+                            <Table.Cell>{invoice.method}</Table.Cell>
+                            <Table.Cell>{invoice.amount}</Table.Cell>
+                            <Table.Cell class="text-right">
+                                <Drawer.Root>
+                                    <Drawer.Trigger asChild let:builder>
+                                      <Button builders={[builder]} variant="outline" class="h-7">View Details</Button>
+                                    </Drawer.Trigger>
+                                    <Drawer.Content>
+                                      <div class="mx-auto w-full max-w-sm">
+                                        <Drawer.Header>
+                                          <Drawer.Title>Transaction: {invoice.invoiceNumber}</Drawer.Title>
+                                          <Drawer.Description>Transaction Status: {invoice.status}</Drawer.Description>
+                                          <Drawer.Description>Transaction Status: {invoice.method}</Drawer.Description>
+                                          <Drawer.Description>Transaction Status: {invoice.amount}</Drawer.Description>
+                                        </Drawer.Header>
+                                        <div class="grid grid-cols-12 p-4 pb-0 w-full">
+                                            <div class="col-span-12 w-full">sdfg2134sdf</div>
+                                        </div>
+                                        <Drawer.Footer>
+                                          <Button>Submit</Button>
+                                          <Drawer.Close asChild let:builder>
+                                            <Button builders={[builder]} variant="outline">Cancel</Button>
+                                          </Drawer.Close>
+                                        </Drawer.Footer>
+                                      </div>
+                                    </Drawer.Content>
+                                  </Drawer.Root>
+                            </Table.Cell>
+                        </Table.Row> 
+                  {/each}
                 </Table.Body>
             </Table.Root>
-            
+        
             <Pagination.Root class="mt-10" count={100} perPage={10} let:pages let:currentPage>
                 <Pagination.Content>
                 <Pagination.Item>
